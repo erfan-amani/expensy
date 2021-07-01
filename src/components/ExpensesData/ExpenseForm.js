c2c43aba9e92ad331d4830ff2db9c532809b4542
erfan-amani <erfan80amani23@gmail.com>
2021-07-01 15:58:08 +0430

    Styled the expenses form

--- src/components/ExpensesData/ExpenseForm.js
+++ src/components/ExpensesData/ExpenseForm.js
@@ -1,7 +1,31 @@
 import React from 'react';
+import './ExpenseForm.css';
 
 const ExpenseForm = () => {
-  return <div>expense form</div>;
+  return (
+    <form className="expenses-form">
+      <div className="expenses-form__title">
+        <label>Title</label>
+        <input type="text" />
+      </div>
+      <div className="expenses-form__date">
+        <label>Date</label>
+        <input type="date" />
+      </div>
+      <div className="expenses-form__amount">
+        <label>Amount</label>
+        <input type="number" />
+      </div>
+      <div className="expenses-form__submit">
+        <a href="." className="cansel">
+          Cansel
+        </a>
+        <button type="submit" className="add">
+          Add
+        </button>
+      </div>
+    </form>
+  );
 };
 
 export default ExpenseForm;
