81682ed3b2a0445b44e145bf2c3bf1dca89d8888
erfan-amani <erfan80amani23@gmail.com>
2021-07-02 15:30:51 +0430

    created filtered expenses by year

--- src/components/ExpensesData/ExpensesList.js
+++ src/components/ExpensesData/ExpensesList.js
@@ -4,21 +4,46 @@ import ExpenseItem from './ExpenseItem';
 import FilterExpenses from './FilterExpenses';
 
 const ExpensesList = ({ items }) => {
-  const [filterYear, setFilterYear] = useState('Filter year');
+  const [filteredYear, setFilteredYear] = useState('Filter year');
+
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
+
+  let renderedExpenses = <p>No expenses found.</p>;
+  if (filteredYear === 'Filter year') {
+    renderedExpenses = items.map((item) => (
+      <ExpenseItem
+        key={item.id}
+        title={item.title}
+        date={item.date}
+        amount={item.amount}
+      />
+    ));
+  } else {
+    const filteredExpenses = items.filter(
+      (item) => item.date.getFullYear().toString() === filteredYear
+    );
+
+    if (filteredExpenses.length > 0) {
+      renderedExpenses = filteredExpenses.map((item) => (
+        <ExpenseItem
+          key={item.id}
+          title={item.title}
+          date={item.date}
+          amount={item.amount}
+        />
+      ));
+    }
+  }
 
   return (
     <>
-      <FilterExpenses />
-      <div className="expenses-list">
-        {items.map((item) => (
-          <ExpenseItem
-            key={item.id}
-            title={item.title}
-            date={item.date}
-            amount={item.amount}
-          />
-        ))}
-      </div>
+      <FilterExpenses
+        selectedYear={filteredYear}
+        filterChangeHandler={filterChangeHandler}
+      />
+      <div className="expenses-list">{renderedExpenses}</div>
     </>
   );
 };
--- src/components/ExpensesData/FilterExpenses.js
+++ src/components/ExpensesData/FilterExpenses.js
@@ -1,19 +1,16 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './FilterExpenses.css';
 
-const FilterExpenses = ({ items }) => {
-  const [selectedYear, setSelectedYear] = useState('Filter year');
-
-  const selectYearHandler = (event) => {
-    setSelectedYear(event.target.value);
-    console.log(event.target.value);
+const FilterExpenses = ({ filterChangeHandler, selectedYear }) => {
+  const yearChangeHandler = (event) => {
+    filterChangeHandler(event.target.value);
   };
 
   return (
     <div className="filter-expenses">
       <h4 className="filter-expenses__header">Expense List</h4>
       <div className="filter-expenses__select">
-        <select onChange={selectYearHandler} value={selectedYear}>
+        <select onChange={yearChangeHandler} value={selectedYear}>
           <option>Filter year</option>
           <option>2021</option>
           <option>2020</option>
